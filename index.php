59719526077b6aaf746b709be9219176f5b2c640
Alessandra Vieira Rogatto <84882618+AlessandraRogatto@users.noreply.github.com>
2022-08-28 01:23:19 -0300

    Update index.php
    Pequenas alterações de adaptação

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "192.168.1.16";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "123";
+$database = "Itens";
 
 // Criar conexão
 
@@ -34,7 +34,7 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "NSERT INTO dados (ProdutoID, NomeItem, DescricaoItem, Preco, Quantidade) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand3', '$valor_rand4', '$valor_rand5')";
 
 
 if ($link->query($query) === TRUE) {
