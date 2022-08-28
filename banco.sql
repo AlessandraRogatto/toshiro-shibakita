3a0511805e9c079cbf7a046d97ea14e654c52abb
Alessandra Vieira Rogatto <84882618+AlessandraRogatto@users.noreply.github.com>
2022-08-28 17:47:48 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,7 +1,6 @@
 CREATE TABLE dados (
-    ProdutoID int,
-    NomeItem varchar(50),
-    DescricaoItem varchar(50),
-    Preco varchar(150),
+    ID int,
+    Nome varchar(50),
+    Preco int,
     Quantidade int
 );
